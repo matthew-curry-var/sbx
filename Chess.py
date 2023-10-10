80f850d10003135e76d62cb705a4d83fc21a3ace
Matthew Curry <matthew_jamesc@berkeley.edu>
2023-10-10 16:10:16 -0700

    piece rec logic

--- Chess.py
+++ Chess.py
@@ -13,20 +13,45 @@ class Chess:
         else:
             self.board.movePiece(xOrig, yOrig, xDest, yDest)
 
+    """getPieceLegalMoves: return list of legal moves according to std chess rules"""
     def getPieceLegalMoves(self, pieceX : int, pieceY : int) -> list:
-        if (self.board.getPiece(pieceX, pieceY) is None):
-            return []
+        piece, moves = self.board.getPiece(pieceX, pieceY), list()
+        if (piece is None):
+            return moves
         else:
-            #need to identify piece abilities quickly
-            #return list of legal moves
-            #what is a legal move?
-            #piece have certain moving capabilities
-            #piece cannot capture peces of same color
-            #piece cannot move off the grid
+            if (piece % 2 != 0): #White piece
+                if (piece == 0x01): #White pawn
+                    pass
+                elif (piece == 0x03): #White rook
+                    pass
+                elif (piece == 0x05): #White knight
+                    pass
+                elif (piece == 0x07): #White bishop
+                    pass
+                elif (piece == 0x09): #White queen
+                    pass
+                else: #White king (== 0x0B)
+                    pass
 
-            pass
+            else: #Black piece
+                if (piece == 0x02): #Black pawn
+                    pass
+                elif (piece == 0x04): #Black rook
+                    pass
+                elif (piece == 0x06): #Black knight
+                    pass
+                elif (piece == 0x08): #Black bishop
+                    pass
+                elif (piece == 0x0A): #Black queen
+                    pass
+                else: #Black king (== 0x0C)
+                    pass
 
-    
+    def isEmpty(self, x, y) -> bool:
+        if (self.board.getPiece(x,y) is None):
+            return True
+        return False
+
     def getAllRemainingPieces(self) -> list:
         pass
 
